2350b1fbb5a3ae45410ebb2fe1215b7e16326548
Layamon <sdwhlym@gmail.com>
2024-02-20 10:42:17 +0800

    update notice

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -34,4 +34,64 @@ return {
 			})
 		end,
 	},
+	{
+		"folke/noice.nvim",
+		opts = {
+			presets = {
+				-- you can enable a preset by setting it to true, or a table that will override the preset config
+				-- you can also add custom presets that you can enable/disable with enabled=true
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = false, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = true, -- enables an input dialog for inc-rename.nvim
+			},
+			routes = {
+				{
+					view = "notify",
+					filter = { event = "msg_showmode" },
+				},
+			},
+			sections = {
+				lualine_x = {
+					{
+						require("noice").api.statusline.mode.get,
+						cond = require("noice").api.statusline.mode.has,
+						color = { fg = "#ff9e64" },
+					}
+				},
+			},
+			views = {
+				cmdline_popup = {
+					position = {
+						row = "50%",
+						col = "50%",
+					},
+					size = {
+						min_width = 60,
+						width = "auto",
+						height = "auto",
+					},
+				},
+				cmdline_popupmenu = {
+					relative = "editor",
+					position = {
+						row = "63%",
+						col = "50%",
+					},
+					size = {
+						width = 60,
+						height = "auto",
+						max_height = 15,
+					},
+					border = {
+						style = "rounded",
+						padding = { 0, 1 },
+					},
+					win_options = {
+						winhighlight = { Normal = "Normal", FloatBorder = "NoiceCmdlinePopupBorder" },
+					},
+				},
+			},
+		}
+	}
 }
