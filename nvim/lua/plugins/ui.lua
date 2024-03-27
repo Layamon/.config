205f9e35ddc5cfe38419275ee96e0604fef0908d
Layamon <sdwhlym@gmail.com>
2024-03-27 17:37:13 +0800

    update neotree

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,5 +1,24 @@
 return {
 	{
+		"nvim-neo-tree/neo-tree.nvim",
+		opts = {
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					show_hidden_count = true,
+					hide_dotfiles = false,
+					hide_gitignored = true,
+					hide_by_name = {
+						-- '.git',
+						-- '.DS_Store',
+						-- 'thumbs.db',
+					},
+					never_show = {},
+				},
+			}
+		}
+	},
+	{
 		"EdenEast/nightfox.nvim",
 		config = function()
 			require("nightfox").setup({
