5af9fdc49c3ad2dbdc3710d93985a29b2d59d3dd
L-YM <sdwhlym@gmail.com>
2025-05-20 14:22:32 +0800

    add mor

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -28,7 +28,7 @@ vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldenable = true
 
-vim.o.background = "light"
+vim.o.background = "dark"
 -- control cursor shape of vim
 --vim.api.nvim_exec2([[
 --let &t_SI = "\<Esc>]50;CursorShape=1\x7"
--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -7,4 +7,7 @@ return {
 		{ "<leader>dc", "<cmd>DiffviewClose<cr>", desc = "Close diffview" },
 		{ "<leader>df", "<cmd>DiffviewFileHistory %<cr>", desc = "Current File History" },
 	},
+	default_args = {
+		DiffviewOpen = { "^!" }, -- 让 :DiffviewOpen 默认为单次提交模式
+	},
 }
