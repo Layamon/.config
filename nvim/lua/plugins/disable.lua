147855109cfd79ac0b42d614bc2795f6da994ee6
L-YM <sdwhlym@gmail.com>
2025-06-06 18:03:30 +0800

    add LuaSnip
    use :
    scl enable devtoolset-11 bash
    to compile

--- nvim/lua/plugins/disable.lua
+++ nvim/lua/plugins/disable.lua
@@ -3,5 +3,4 @@ return {
   { "echasnovski/mini.comment", enabled = false },
   { "folke/neodev.nvim", enabled = false },
   { "JoosepAlviste/nvim-ts-context-commentstring", enabled = false },
-  { "L3MON4D3/LuaSnip", enabled = false },
 }
