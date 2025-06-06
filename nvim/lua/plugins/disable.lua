4062003432392b56062fbab3eb3cf1f2732ea315
L-YM <sdwhlym@gmail.com>
2025-06-06 17:32:58 +0800

    remove some unused plugin

---
+++ nvim/lua/plugins/disable.lua
@@ -0,0 +1,7 @@
+return {
+  { "saadparwaiz1/cmp_luasnip", enabled = false },
+  { "echasnovski/mini.comment", enabled = false },
+  { "folke/neodev.nvim", enabled = false },
+  { "JoosepAlviste/nvim-ts-context-commentstring", enabled = false },
+  { "L3MON4D3/LuaSnip", enabled = false },
+}
